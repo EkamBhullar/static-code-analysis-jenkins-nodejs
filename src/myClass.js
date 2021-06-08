e9be3d6bfc12f3ae0a4b055a49a0d9a6d278565c
Shahzeb <shah10zeb@gmail.com>
2021-06-08 20:26:19 +0530

    first commit

---
+++ src/myClass.js
@@ -0,0 +1,12 @@
+class MyClass {
+    constructor() {
+        console.log("in constructor")
+    }
+    add(arg1, arg2) {
+        var result;
+        result = arg1 + arg2;
+        return result;
+    }
+}
+
+module.exports = MyClass;
