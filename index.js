82a1ca19944465ad222f0763ef041d807d0fd033
shah10zeb <31175007+shah10zeb@users.noreply.github.com>
2021-06-10 14:44:50 +0530

    Updated indentation in ( 4 -> 2)

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var http = require("http");
 
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello NodeApp!! \n");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello NodeApp!! \n");
 });
 
 // listen on localhost:8000
